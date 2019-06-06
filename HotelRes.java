6e9a0b5788dac38663c177065b476f1bfe0e0106
Christopher McCoy <35251302+cmccoy13@users.noreply.github.com>
2019-06-06 12:41:56 -0700

    Added Basic UI

--- HotelRes.java
+++ HotelRes.java
@@ -1,5 +1,6 @@
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Scanner;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.DriverManager;
@@ -13,6 +14,9 @@ export APP_JDBC_PW=008506325
 */
 
 public class HotelRes {
+	
+	static Scanner sc = new Scanner(System.in);
+	
     public static void main(String[] args) {
 
         try{
@@ -27,7 +31,6 @@ public class HotelRes {
         String dbUsername = System.getenv("cmmccoy");
         String dbPassword = System.getenv("008506325");
 
-
         try {
             Connection conn = DriverManager.getConnection(jdbcUrl, dbUsername, dbPassword);
             System.out.print("MySQL Connection created");
@@ -36,5 +39,66 @@ public class HotelRes {
             e.printStackTrace();
 
         }
+        
+        String command = "";
+		
+		while(command != "q") {
+			printWelcome();
+			command = sc.nextLine();
+			executeCommand(command);
+		}		
     }
+    
+    private static void printWelcome() {
+		System.out.println("\nPlease enter a command from the following list\n\n"
+				+ "1: Book a room\n"
+				+ "2: Cancel reservation\n"
+				+ "3: Change reservation\n"
+				+ "4: Reservation history\n"
+				+ "5: Manager sign-in\n\n"
+				+ "q: quit");
+	}
+	
+	private static void executeCommand(String command) {
+		if(command.equals("1")) {
+			startBooking();
+		}
+		else if(command.equals("2")) {
+			startCancelRes();
+		}
+		else if(command.equals("3")) {
+			startChangeRes();
+		}
+		else if(command.equals("4")) {
+			startResHistory();
+		}
+		else if(command.equals("5")) {
+			startManager();
+		}
+		else {
+			System.out.println("Invalid Selection. Please input a valid command.");
+			command = sc.next();
+			executeCommand(command);
+		}
+	}
+	
+	private static void startBooking() {
+		System.out.println("Starts the flow for a user to book a room");
+	}
+	
+	private static void startCancelRes() {
+		System.out.println("Starts the flow for a user to cancel their reservation");
+	}
+
+	private static void startChangeRes() {
+		System.out.println("Starts the flow for a user to change their reservation");
+	}
+
+	private static void startResHistory() {
+		System.out.println("Starts the flow for a user to view their reservation history");
+	}
+
+	private static void startManager() {
+		System.out.println("Allows a manager to sign in to view revenue of year");
+	}
 }
